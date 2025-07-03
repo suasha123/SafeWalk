dc009aeab214ed111a2463e036665024ef30ca9c
suasha123 <suryaprataps471@gmail.com>
2025-07-03 13:25:51 +0530

    Added group schema

---
+++ Backend/database/model/GroupSchema.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const GroupSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    member: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+      },
+    ],
+    invitecode: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Group", GroupSchema);
