04a8c44ab29704063cae10e0ee3d0fbcdd124f46
suasha123 <suryaprataps471@gmail.com>
2025-07-05 23:12:43 +0530

    Finally done with personal dm and group chat

--- Backend/Routes/userinfo.js
+++ Backend/Routes/userinfo.js
@@ -93,7 +93,7 @@ router.get("/groupmsg/:groupId", async (req, res) => {
     }
 
     const messages = await GroupChatModel.find({ group: groupId })
-      .populate("from", "name")
+      .populate("from", "name profile")
       .sort({ createdAt: 1 });
 
     const formatted = messages.map((msg) => ({
