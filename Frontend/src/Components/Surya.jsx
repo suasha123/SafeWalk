8f1270cc88acf5b03f751434121c9186f9c0ab60
suasha123 <suryaprataps471@gmail.com>
2025-07-18 16:55:03 +0530

    Fixed Map thing

--- Frontend/src/Components/Surya.jsx
+++ Frontend/src/Components/Surya.jsx
@@ -1,10 +1,19 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import { useEffect, useState } from "react";
-
+import L from "leaflet";
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
 export const Surya = () => {
   const [pos, setLoc] = useState(null);
-
+  const [loading , setLoading] = useState(true);
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(
       (pos) => {
@@ -17,18 +26,22 @@ export const Surya = () => {
         console.error("Geolocation error:", err.message);
       },
       {
-    enableHighAccuracy: true,
-    timeout: 10000,
-    maximumAge: 0,
-  }
+        enableHighAccuracy: true,
+        timeout: 10000,
+        maximumAge: 0,
+      }
     );
   }, []);
+  useEffect(()=>{
+    if(pos){
+        setLoading(false);
+    }
+  },[pos])
 
   return (
-  <div style={{ height: "500px", width: "100%" }}>
-    {pos && (
-      <MapContainer
-        center={pos}
+    <div style={{ height: "500px", width: "100%" }}>
+    {loading?"Loading Map": (<MapContainer
+        center={pos != null ? pos : [51.505, -0.09]}
         zoom={13}
         scrollWheelZoom={true}
         style={{ height: "100%", width: "100%" }}
@@ -37,11 +50,10 @@ export const Surya = () => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={pos}>
+        <Marker position={pos != null ? pos : [51.505, -0.09]}>
           <Popup>You are here!</Popup>
         </Marker>
-      </MapContainer>
-    )}
-  </div>
+      </MapContainer>)}
+    </div>
   );
 };
