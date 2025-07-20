178140d03208cc62f5c67bc18e585a3c5c8c7de7
suasha123 <suryaprataps471@gmail.com>
2025-07-21 02:56:56 +0530

    jnjn

--- Backend/Routes/Search.js
+++ Backend/Routes/Search.js
@@ -8,7 +8,7 @@ router.get("/searchPlace", async (req, res) => {
   }
   try {
     const response = await fetch(
-      `https://us1.locationiq.com/v1/search.php?key=${process.env.SEARCHTOKEN}&q=${encodeURIComponent(location)}&format=json`
+      `https://api.locationiq.com/v1/autocomplete?key=${process.env.SEARCHTOKEN}&q=${encodeURIComponent(location)}&limit=5&dedupe=1&`
     );
     const result = await response.json();
     return res.status(200).json(result);
